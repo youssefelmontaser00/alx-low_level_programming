b15ee09e0089fe5e538203eff4df49fd005ff775
Youssef EL Montaser <youssefelmontaser00@gmail.com>
2023-03-16 15:03:11 -0700

    #include <stdio.h>

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line with puts function
- * Return: 0 (Success)
- * /
+ * main - 4-puts.c
+ * description - Write a C program that prints exactly
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-puts("\"programming is like building a multilingual puzzle");
-return (0);
+        puts("\"Programming is like building a multilingual puzzle");
+
+        return (0);
 }
