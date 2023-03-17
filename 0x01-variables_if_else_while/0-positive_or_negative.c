c52f7a32db48673cf0f5d5bf62b86d28ae5f1154
Youssef EL Montaser <youssefelmontaser00@gmail.com>
2023-03-17 14:27:23 -0700

    if statement

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,34 +1,30 @@
 /*
  * File: 0-positive_or_negative.c
  * Auth: Youssef EL Montaser
- * Description: states whather is positive or negative or zero
  */
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
- * main - Entry point
+ * main - Prints a readom number and states whather
+ * 	it is positive, negative, or zero
  *
- * Description: 'the program's description'
- * @parameter: describe the parameter
- *
- * Return:Always 0 (success)
+ * Return:Always 0.
  */
-
-
-
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if(n>0)
-		printf("@d is positive\n",n);
-	else if(n < 0)
-		printf("@d is negative\n",n);
+
+	if (n > 0)
+		printf("@d is positive\n", n);
+	else if (n < 0)
+		printf("@d is negative\n", n);
 	else
-		printf("@d is zero\n",n);
+		printf("@d is zero\n", n);
 
 	return (0);
 }
