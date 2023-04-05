69e45e9d81a265b8927c28cfc54fa20b8af0edb0
youssefelmontaser00 <youssefelmontaserzero@gmail.com>
2023-04-05 15:00:48 +0000

    0x08. C - Recursion done

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -20,3 +20,14 @@ int str_checker(char *s1, char *s2, int i, int j)
 		return (str_checker(s1, s2, i + 1, j) || str_checker(s1, s2, i, j + 1));
 	return (0);
 }
+/**
+ * wildcmp - check if strings could be considered identical
+ * @s1: base address for string.
+ * @s2: base address for string.
+ *
+ * Return: 1 if are considered identical.
+ */
+int wildcmp(char *s1, char *s2)
+{
+	return (str_checker(s1, s2, 0, 0));
+}
