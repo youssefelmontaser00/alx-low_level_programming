cfbfdc8f8d89d2f010b107a01f2924899ad30960
Youssef EL Montaser <youssefelmontaserzero@gmail.com>
2023-04-19 12:00:13 -0700

    0x0F. C - Function pointers TASKS

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,23 +1,24 @@
-#include <stdio.h>
 #include "function_pointers.h"
+#include <stdlib.h>
+#include <stddef.h>
 
 /**
- * array_iterator - prints an integer
- * @array: is pointer to int
- * @action: is a pointer to the function
- * @size: is the size of the array
- *
- * Return: Nothing.
-**/
+ * array_iterator - executes a function given as a parameter
+ * on each element of an array
+ * @array: pointer to an array of integers
+ * @size: size of the array
+ * @action: pointer to the function to be executed on
+ * each element of the array
+ * *
+ * Return: void
+ */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-size_t i;
-if (array == NULL || action == NULL)
-{
-return;
-}
-for (i = 0; i < size; i++)
-{
-action(array[i]);
-}
+	unsigned int i;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
 }
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,4 +3,4 @@
 
 void print_name(char *name, void (*f)(char *));
 
-#endif /* FUNCTION_POINTERS_H */
+#endif /* FUNCTION_POINTERS_H *
